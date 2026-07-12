3b7d42872f1a306b6919ab5c4bee7035aea9591f
Marwan Mohamed <Marwan.habib22607@gmail.com>
2026-07-12 23:00:17 +0300

    Update Assignment 2 basic solution

--- Assignment_02_String_Manipulator/basic/string_manipulator.c
+++ Assignment_02_String_Manipulator/basic/string_manipulator.c
@@ -1,37 +1,52 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int vowelCount = 0;
+void modifyString(char (*str)[50]);
+static int vcount = 0; 
 
-void modifyString(char *str)
-{
-    while (*str != '\0')
-    {
-        if (*str == 'a' || *str == 'e' || *str == 'i' || *str == 'o' || *str == 'u' ||
-            *str == 'A' || *str == 'E' || *str == 'I' || *str == 'O' || *str == 'U')
-        {
-            vowelCount++;
-        }
+int main(){
 
-        if (*str >= 'a' && *str <= 'z')
-        {
-            *str = *str - ('a' - 'A');
-        }
+    char word[50];
+    printf("Enter the Word: ") ;
+    scanf("%s",word) ;
+    modifyString(&word) ;
 
-        str++;
+    printf("Your Word Traversed: \n");
+    int act_len = 0 , check = 0;
+    while((word[check] !=  '\0' && act_len < sizeof(word) )){
+            ++act_len;
+            ++check ;  
     }
+    for(int i = 0 ; i < act_len ; i++){
+        printf("%c", word[i]);      
+    }
+    printf("\nNumber of Vowels: %d" , vcount) ;
 }
 
-int main()
-{
-    char str[100];
+void modifyString(char (*str)[50]){
+    
+    int act_len = 0 ;
+    int check = 0 ;
+    
+    while(*(*str+check) !=  '\0' && act_len < sizeof(*str) ){
+            ++act_len; 
+            ++check ; 
+    }
+    char temp ;
+    for(int i = 0 ; i < act_len/2 ; i++){
+        temp = *(*str + i) ;
+        *(*str + i) = *(*str + act_len - 1 -i) ;
+        *(*str + act_len - 1 -i) = temp ;
+    }
+    for(int i = 0 ; i < act_len ; i++){
+        if(*(*str + i) > 96){
+            *(*str+i) -= 32;
+        } 
 
-    printf("Enter a word: ");
-    scanf("%99s", str);
-
-    modifyString(str);
-
-    printf("Modified String: %s\n", str);
-    printf("Vowel Count: %d\n", vowelCount);
-
-    return 0;
+        if(*(*str+i)=='a' || *(*str+i)=='A' || *(*str+i)=='e' || *(*str+i)=='E' || *(*str+i)=='o' || *(*str+i)=='O' || *(*str+i)=='i' || *(*str+i)=='I' || *(*str+i)=='u' || *(*str+i)=='U' || *(*str+i)=='y' || *(*str+i)=='Y'){
+            vcount++ ;
+        } 
+        else 
+            continue;
+    }
+    
 }
